f771a3ffbb89ce89ca56067b32b6306d0aa619c0
underht <ntr4000plus@gmail.com>
2026-09-07 20:31:20 +0800

    feat user store

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,15 +1,27 @@
 import { ref } from "vue";
 import { defineStore } from "pinia";
 export const useUserStore = defineStore("user", () => {
-    const username=ref('');
-    const password=ref('');
-    function setUser(user){
-        username.value=user.username;
-        password.value=user.password;
+    const username = ref('');
+    const token = ref('');
+    const role = ref('');
+
+    function setUser(user) {
+        username.value = user.username;
+        token.value = user.token;
+        role.value = user.role;
     }
-  return {
-    username,
-    password,
-    setUser
-  };
+
+    function clearUser() {
+        username.value = '';
+        token.value = '';
+        role.value = '';
+    }
+
+    return {
+        username,
+        token,
+        role,
+        setUser,
+        clearUser
+    };
 });
